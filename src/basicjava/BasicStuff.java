40340acdd32bca33ee4a9b97a82a2cce94ca2973
DucNgo1822 <dngo@macalester.edu>
2019-09-06 21:08:05 -0500

    This is homework 0 of Duc Ngo. In here, I have changed the formula to match the requirement for the homework.

---
+++ src/basicjava/BasicStuff.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class BasicStuff {
+    public static void main(String[] args) {
+        double age0 = 18;
+        double age1 = 19;
+        double sumOfAges = age0 + age1;
+        System.out.println("We are " + age0 + " and " + age1 + " years old!");
+        System.out.println("The sum of our ages is " + sumOfAges);
+        System.out.println("The sum of our ages is " + (age0 + age1));
+    }
+}
